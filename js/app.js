2538ccbaf676d04a6651745ad60b5b947ab62efa
MuhammadAtoum <muhammadatoum@gmail.com>
2021-03-15 21:31:36 +0200

    lab02

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+alert('welcome to my page');
+
+
+
+
+is that first time you visit my page??
+so i hope you will enjoy
+
+
+what is your name
+you are welcome
+
+
+
+is my age 26??
+will done next step click ok
+
+
+dose muhammad work for the Save the Children organization?
+Good job two questions left
+
+
+is one of favorite hobbies by muhammad are programming?
+
+okay the last questions
+
+
+
+
