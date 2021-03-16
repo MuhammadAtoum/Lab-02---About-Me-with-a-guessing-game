0dfa922691ed47617a2b2e580456b6e8867e3657
MuhammadAtoum <muhammadatoum@gmail.com>
2021-03-16 03:42:49 +0200

    lab02 game

--- js/app.js
+++ js/app.js
@@ -1,31 +1,41 @@
 'use strict';
 
+
+let name = prompt('what is your name ?');
+alert('will done thank you'+' '+ name);
+console.log("user Name", name);
+
+
+
+let start  = prompt('is that first time you visit my page ??');
+alert('enjoy'+' '+ name);
+console.log('visit',start.toLowerCase())
+
+
+
+let age = prompt('is your age 26??');
+alert('keep going'+' '+ name);
+console.log('the age',age.toLowerCase());
+
+
+
+let work = prompt('do you work for the Save the Children organization?');
+alert('Good job two questions left'+' '+name);
+console.log('SOjob',work.toLowerCase());
+
+
+
+let hope = prompt('do you love porograming');
+alert('Press OK to contnue');
+console.log('like porograming',hope.toLowerCase());
+
+
+
+
+let animal = prompt('do you love catS??');
+alert('will done'+' '+ name);
+console.log('love pets',animal.toUpperCase());
+
+
+
 alert('welcome to my page');
-
-
-
-
-is that first time you visit my page??
-so i hope you will enjoy
-
-
-what is your name
-you are welcome
-
-
-
-is my age 26??
-will done next step click ok
-
-
-dose muhammad work for the Save the Children organization?
-Good job two questions left
-
-
-is one of favorite hobbies by muhammad are programming?
-
-okay the last questions
-
-
-
-
