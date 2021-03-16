cec8ff0c6e9ef650c580cb1dfbbace0e14972561
MuhammadAtoum <muhammadatoum@gmail.com>
2021-03-16 13:19:58 +0200

    lab02

--- js/app.js
+++ js/app.js
@@ -1,41 +1,43 @@
 'use strict';
 
+alert('welcome to my page');
+
 
 let name = prompt('what is your name ?');
 alert('will done thank you'+' '+ name);
-console.log("user Name", name);
+console.log('user Name', name);
 
 
 
-let start  = prompt('is that first time you visit my page ??');
-alert('enjoy'+' '+ name);
-console.log('visit',start.toLowerCase())
+let start  = prompt('does muhammad visited petra ??');
+alert('Nice '+' '+ name);
+console.log('visit',start.toLowerCase());
 
 
 
-let age = prompt('is your age 26??');
+let age = prompt('is my age 26??');
 alert('keep going'+' '+ name);
 console.log('the age',age.toLowerCase());
 
 
 
-let work = prompt('do you work for the Save the Children organization?');
+let work = prompt('does i worked for the Save the Children organization?');
 alert('Good job two questions left'+' '+name);
 console.log('SOjob',work.toLowerCase());
 
 
 
-let hope = prompt('do you love porograming');
+let hope = prompt('does muhammad  love porograming');
 alert('Press OK to contnue');
 console.log('like porograming',hope.toLowerCase());
 
 
 
 
-let animal = prompt('do you love catS??');
+let animal = prompt('does muhammad love catS??');
 alert('will done'+' '+ name);
 console.log('love pets',animal.toUpperCase());
 
 
 
-alert('welcome to my page');
+
